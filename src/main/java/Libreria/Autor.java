5e7c2c2c2934becafa43cf13d5af8551c6e4888b
qantuw <142445210+qantuw@users.noreply.github.com>
2024-10-21 23:29:35 -0500

    Add files via upload

---
+++ src/main/java/Libreria/Autor.java
@@ -0,0 +1,20 @@
+package Libreria;
+
+public class Autor {
+
+    private String nombre;
+
+    public Autor(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+  
+}
---
+++ src/main/java/Libreria/LibroFisico.java
@@ -0,0 +1,77 @@
+package Libreria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibroFisico extends Libro {
+
+    private int numeroPaginas;
+    private double altura;
+    private double ancho;
+    private double grosor;
+
+    private LibroFisico(Builder builder) {
+        super(builder.titulo, builder.autores, builder.resenas);
+        this.numeroPaginas = builder.numeroPaginas;
+        this.altura = builder.altura;
+        this.ancho = builder.ancho;
+        this.grosor = builder.grosor;
+    }
+
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public double getGrosor() {
+        return grosor;
+    }
+
+    public static class Builder {
+        private String titulo;
+        private List<Autor> autores = new ArrayList<>();
+        private List<Resena> resenas = new ArrayList<>();
+        private int numeroPaginas;
+        private double altura;
+        private double ancho;
+        private double grosor;
+
+        public Builder(String titulo) {
+            this.titulo = titulo;
+        }
+
+        public Builder addAutor(Autor autor) {
+            this.autores.add(autor);
+            return this;
+        }
+
+        public Builder addResena(Resena resena) {
+            this.resenas.add(resena);
+            return this;
+        }
+
+        public Builder setNumeroPaginas(int numeroPaginas) {
+            this.numeroPaginas = numeroPaginas;
+            return this;
+        }
+
+        public Builder setDimensiones(double altura, double ancho, double grosor) {
+            this.altura = altura;
+            this.ancho = ancho;
+            this.grosor = grosor;
+            return this;
+        }
+
+        public LibroFisico build() {
+            return new LibroFisico(this);
+        }
+    }
+}
